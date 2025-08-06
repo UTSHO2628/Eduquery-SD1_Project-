00c43382416443492a27525ab9613585dd42ef55
Utsho Kumar Dey <utshokumerdey@gmail.com>
2025-08-07 00:12:57 +0600

    Update upload.php

--- results/upload.php
+++ results/upload.php
@@ -1,18 +1,18 @@
 <?php
-include '../includes/auth.php';
-include '../includes/db.php';
+include '../includes/auth.php'; // auth.php check user er authentication plus login kora asekina role ki aisob check kore
+include '../includes/db.php';  // database collection 
 
-if ($_SESSION['role'] !== 'teacher') {
+if ($_SESSION['role'] !== 'teacher') {  // check kore user teacher kina
     header("Location: ../login.php");
     exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { // check kore form submit hoise kina 
     $student_id = $_POST['student_id'];
     $subject = $_POST['subject'];
     $marks = $_POST['marks'];
 
-    $stmt = $conn->prepare("INSERT INTO results (student_id, subject, marks, teacher_id) VALUES (?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO results (student_id, subject, marks, teacher_id) VALUES (?, ?, ?, ?)"); // MySQLi prepared statement use kora hoise. prepare() SQL injection theke save korar jonne secure input handling।
     $stmt->bind_param("isii", $student_id, $subject, $marks, $_SESSION['user_id']);
     $stmt->execute();
     echo "Result uploaded!";
