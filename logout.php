41465763d98794804493a4d77344dd52c6d94058
Utsho Kumar Dey <utshokumerdey@gmail.com>
2026-01-28 21:34:32 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'includes/init.php'; // Use the new init.php
 
-session_destroy();  //ata puro session remove kore dei . aita logout er mul kaj 
+session_destroy();  // ata puro session remove kore dei . aita logout er mul kaj 
 header('Location: login.php');
 exit();
 ?>
