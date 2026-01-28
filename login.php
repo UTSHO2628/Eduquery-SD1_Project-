b9525f5b6b31f160ce8811457011b50417a9b1de
Utsho Kumar Dey <utshokumerdey@gmail.com>
2026-01-28 21:35:07 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'includes/init.php'; // Use the new init.php
+include_once 'includes/init.php';  // Use the new init.php..
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     $email = $_POST['email'];
